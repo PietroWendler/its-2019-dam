118762479da75a2122c4a03d0f80c00673ef9413
Andrea Dottor <andrea@dottor.net>
2019-02-07 17:09:37 +0100

    esercizio calc.js

--- 20190207-Intro-JavaScript/intro.js
+++ 20190207-Intro-JavaScript/intro.js
@@ -44,3 +44,115 @@ list2.push("ciao");
 list2[10] = "Pippo";
 
 var list3 = [];
+
+// ==== funzioni ====
+
+function add(num1, num2){
+    console.log(arguments);
+    // vale false se num2 è:
+    // undefined
+    // null
+    // 0
+    // ""
+    // false
+    if(num2)
+        return num1 + num2;
+
+    return num1;
+}
+
+var r1 = add(2, 4);
+var r2 = add(2, 4, 89);
+var r3 = add(2);
+
+
+var log = function(message){
+    console.log(message);
+}
+
+log("ciao");
+
+function add2(num1, num2, success, error){
+    try {
+        var r = num1 + num2;
+        success(r);
+    } catch (ex) {
+        error(ex);
+    }
+}
+
+add2(12, 32, function(result){
+    console.log("OK " + result);
+}, function(err){
+    console.log("Errore");
+});
+
+
+// ===== oggetti ====
+
+var user1 = new Object();
+user1.name = "Andrea";
+user1.surname = "Dottor";
+
+var user2 = new Object();
+user2["name"] = "Andrea";
+user2["surname"] = "Dottor";
+
+function User(name, surname){
+    this.name = name;
+    this.surname = surname;
+    this.getFullName = function(){
+        return this.name + " " + this.surname;
+    }
+}
+
+var user3 = new User("Andrea", "Dottor");
+var user4 = new User("Lorenzo", "Dottor");
+var fullName = user3.getFullName();
+
+var user5 = {
+    name: "Andrea",
+    surname: "Dottor"
+};
+
+
+// ===== costrutti ====
+for(var i=0; i < 100; i++){
+    console.log(i);
+}
+
+for(var i=100; i < 0; i--){
+
+}
+
+// prima eseguo il codice
+// poi testo la condizione.
+// almeno una volta viene sempre eseguito
+var y = 0;
+do {
+
+    y++;
+} while (y < 100);
+
+// prima testo la condizione
+// poi eseguo il codice se la condizione è vera
+while (y < 200) {
+    
+    y++;
+}
+
+var color = "red";
+switch (color) {
+    case "white":
+        console.log("BIANCO");
+        break;
+    case "blue":
+        console.log("BLU");
+        break;
+    case "red":
+        console.log("ROSSO");
+        break;
+    default:
+        console.log("non trovato");
+        break;
+}
--- 20190207-Intro-JavaScript/readme.md
+++ 20190207-Intro-JavaScript/readme.md
@@ -1,10 +1,29 @@
 ## Codice della lezione del 2019.02.07
 
 - introduzione a JavaScript
+- commenti
 - variabili
 - array
 - operatori
 - funzioni
 - costrutti
 
-### esercitazione
+### Esercizio Calc.js
+
+Realizzare una funzione che crei un oggetto per eseguire un calcolo matematico.
+
+Implementare il codice di Calc in modo che il seguente blocco funzioni:
+
+```javascript
+var op = new Calc(123, 45, "+");
+console.log(op.exec()); // 168
+
+var op = new Calc(123, 45, "-");
+console.log(op.exec()); // 78
+
+var op = new Calc(123, 45, "*");
+console.log(op.exec()); // 5535
+
+var op = new Calc(123, 45, "/");
+console.log(op.exec()); // 2.733333333333333
+```
